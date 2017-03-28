0b5aefd940c0134eb4ae49a87654f4a6e8e76de8
frank12331 <ajfrank12331@gmail.com>
2017-03-28 21:22:33 +0800

    Update app.js

--- js/app.js
+++ js/app.js
@@ -14,30 +14,35 @@ var src_content = ["Secure Financial Services, Inc. provides hight quality trade
 	"You can use fintech everywhere, everytimes.",
 	"Nowadays, our internet is faster and faster, so is the fintech.",
 	"Fintech is designed by many professional engineer to make your life smarter."]
-	function dark(he){
-		var temp;
-		var check=0;
-		for(var i=0;i<5;i++){
-			if(he.attr('src')==src_btight[i]){	
-				check=1;
-				temp=i;
-			}
-		}
-		if(check==1){
-			he.attr('src',src_dark[temp]);
+function dark(he){
+	var temp;
+	var check=0;
+	for(var i=0;i<5;i++){
+		if(he.attr('src')==src_btight[i]){	
+			check=1;
+			temp=i;
 		}
 	}
+	if(check==1){
+		he.attr('src',src_dark[temp]);
+	}
+}
 $('.feature_icon').find('img').on('mouseenter',function(){
-		var temp;
-		var check=0;
-		for(var i=0;i<5;i++){
-			if($(this).attr('src')==src_dark[i]||$(this).attr('src')==src_bright[i]){
-				
-				check=1;
-				temp=i;
-				dark($(this).siblings(i));
-			}
-			else{
-				dark($('#'+i));
-			}
+	var temp;
+	var check=0;
+	for(var i=0;i<5;i++){
+		if($(this).attr('src')==src_dark[i]||$(this).attr('src')==src_bright[i]){			
+			check=1;
+			temp=i;
+			dark($(this).siblings(i));
 		}
+		else{
+			dark($('#'+i));
+		}
+	}
+	if(check==1){
+		$(this).attr('src',src_l[temp]);
+		$('.p2_c_d_p1').text(src_t[temp]);
+		$('.p2_c_d_p2').data("kind",temp);
+		}
+	};
