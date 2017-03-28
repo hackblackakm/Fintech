b5ff2c09b46e59fd20bd7ee89edb95edde745bc8
frank12331 <ajfrank12331@gmail.com>
2017-03-28 21:19:51 +0800

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,9 +1,37 @@
 // open
+var src_bright = ["img/cheap1-03.png", 
+		  "img/secure1-02.png",
+		  "img/convenient1-05.png",
+		  "img/fast1-04.png",
+		  "img/smart1-01.png"];
+var src_dark = ["img/cheap-03.png",
+		"img/secure-02.png",
+		"img/convenient-05.png",
+		"img/fast-04.png",
+		"img/smart-01.png"];
+var src_content = ["Secure Financial Services, Inc. provides hight quality trade analysis reports, starting with the standard profit and loss analysis, for targeted legal response.",
+	"The fintech net have been proved extremely safe.",
+	"You can use fintech everywhere, everytimes.",
+	"Nowadays, our internet is faster and faster, so is the fintech.",
+	"Fintech is designed by many professional engineer to make your life smarter."]
+	function dark(he){
+		var temp;
+		var check=0;
+		for(var i=0;i<5;i++){
+			if(he.attr('src')==src_btight[i]){	
+				check=1;
+				temp=i;
+			}
+		}
+		if(check==1){
+			he.attr('src',src_dark[temp]);
+		}
+	}
 $('.feature_icon').find('img').on('mouseenter',function(){
 		var temp;
 		var check=0;
 		for(var i=0;i<5;i++){
-			if($(this).attr('src')==src_d[i]||$(this).attr('src')==src_l[i]){
+			if($(this).attr('src')==src_dark[i]||$(this).attr('src')==src_bright[i]){
 				
 				check=1;
 				temp=i;
