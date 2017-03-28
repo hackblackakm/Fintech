8f2f16106a477a6ed063675925f6b5c9410fa87a
frank12331 <ajfrank12331@gmail.com>
2017-03-28 21:07:54 +0800

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,15 @@
 // open
+$('.feature_icon').find('img').on('mouseenter',function(){
+		var temp;
+		var check=0;
+		for(var i=0;i<5;i++){
+			if($(this).attr('src')==src_d[i]||$(this).attr('src')==src_l[i]){
+				
+				check=1;
+				temp=i;
+				dark($(this).siblings(i));
+			}
+			else{
+				dark($('#'+i));
+			}
+		}
